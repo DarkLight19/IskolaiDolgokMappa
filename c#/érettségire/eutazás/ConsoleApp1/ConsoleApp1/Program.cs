f98a6cc03ea7352100b26f933b3bf4f5d523a5b5
Imre <19darklight@gmail.com>
2022-03-01 14:02:22 +0100

    2022.03.01 - 14:02

---
+++ c#/érettségire/eutazás/ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,126 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        class Adat
+        {
+            public int megalloSorszam { get; set; }
+            public DateTime datum { get; set; }
+            public int azonosito { get; set; }
+            public string tipus { get; set; }
+            public (bool, int, DateTime) jegyDBvagyLejaratiDatum { get; set; }
+            public Adat()
+            {
+
+            }
+            public Adat(int a, DateTime b, int c, string d, (bool, int, DateTime) e)
+            {
+                megalloSorszam = a;
+                datum = b;
+                azonosito = c;
+                tipus = d;
+                jegyDBvagyLejaratiDatum = e;
+            }
+        }
+        static void Main(string[] args)
+        {
+            List<Adat> x = new List<Adat>();
+            foreach (var item in System.IO.File.ReadAllLines("utasadat.txt"))
+            {
+                string[] s = item.Split(' ');
+                Adat a = new Adat();
+                a.megalloSorszam = int.Parse(s[0]);
+                a.datum = new DateTime(int.Parse(s[1].Substring(0, 4)), int.Parse(s[1].Substring(4, 2)), int.Parse(s[1].Substring(6, 2)), int.Parse(s[1].Substring(9, 2)), int.Parse(s[1].Substring(11)), 0);
+                a.azonosito = int.Parse(s[2]);
+                a.tipus = s[3];
+                if (s[3] == "JGY")
+                    a.jegyDBvagyLejaratiDatum = (true, int.Parse(s[4]), new DateTime());
+                else
+                    a.jegyDBvagyLejaratiDatum = (false, -1, new DateTime(int.Parse(s[4].Substring(0, 4)), int.Parse(s[4].Substring(4, 2)), int.Parse(s[4].Substring(6, 2))));
+                
+                x.Add(a);
+            }
+
+            //2. feladat:
+            Console.WriteLine("2. feladat:");
+            Console.WriteLine("A buszra " + x.Select(k=>k.azonosito).Distinct().Count() + " utas akart felszállni.");
+
+            //3. feladat:
+            Console.WriteLine("3. feladat:");
+            int count = x.Where(k => k.jegyDBvagyLejaratiDatum.Item1== true && k.jegyDBvagyLejaratiDatum.Item2 == 0).Count();
+            foreach (var item in x.Where(k => k.jegyDBvagyLejaratiDatum.Item1 == false))
+                //if (item.datum > item.jegyDBvagyLejaratiDatum.Item3)
+                if (new DateTime(item.datum.Year, item.datum.Month, item.datum.Day).CompareTo(item.jegyDBvagyLejaratiDatum.Item3) > 0)
+                    ++count;
+
+            Console.WriteLine("A buszra " + count + " utas nem szállhatott fel.");
+
+            //4. feladat:
+            Console.WriteLine("4. feladat:");
+            int max = 0;
+            int maxhely = 0;
+            for (int i = 0; i < x.Select(k => k.megalloSorszam).Distinct().Count(); i++)
+            {
+                int temp = x.Where(k => k.megalloSorszam == i).Count();
+                if(temp >max)
+                {
+                    max = temp;
+                    maxhely = i;
+                }
+            }
+            Console.WriteLine("A legtöbb utas (" + max + " fő) a " + maxhely + ". megállóban próbált felszállni.");
+
+            //5. feladat:0
+            Console.WriteLine("5. feladat:");
+
+            //ingyenes
+            count = x.Where(k => (k.tipus == "NYP" || k.tipus == "RVS" || k.tipus == "GYK") && k.jegyDBvagyLejaratiDatum.Item1 == true && k.jegyDBvagyLejaratiDatum.Item2 > 0).Count();
+            foreach (var item in x.Where(k => (k.tipus == "NYP" || k.tipus == "RVS" || k.tipus == "GYK") && k.jegyDBvagyLejaratiDatum.Item1 == false))
+                //if (item.datum <= item.jegyDBvagyLejaratiDatum.Item3)
+                if (new DateTime(item.datum.Year, item.datum.Month, item.datum.Day).CompareTo(item.jegyDBvagyLejaratiDatum.Item3) <= 0)
+                    ++count;
+
+            Console.WriteLine("Ingyenesen: " + count);
+
+            //kedvezményes:
+            count = x.Where(k => (k.tipus == "TAB" || k.tipus == "NYB" ) && k.jegyDBvagyLejaratiDatum.Item1 == true && k.jegyDBvagyLejaratiDatum.Item2 > 0).Count();
+            foreach (var item in x.Where(k => (k.tipus == "TAB" || k.tipus == "NYB") && k.jegyDBvagyLejaratiDatum.Item1 == false))
+                //if (item.datum <= item.jegyDBvagyLejaratiDatum.Item3)
+                if (new DateTime(item.datum.Year, item.datum.Month, item.datum.Day).CompareTo(item.jegyDBvagyLejaratiDatum.Item3) <= 0)
+                    ++count;
+
+            Console.WriteLine("Kedvezményes: " + count);
+
+            //6. feladat:
+            Console.WriteLine("6. feladat:");
+
+            //7. feladat:
+            Console.WriteLine("7. feladat:");
+            using (System.IO.StreamWriter f = new System.IO.StreamWriter("figyelmeztetes.txt"))
+            {
+                foreach (var item in x.Where(k => k.jegyDBvagyLejaratiDatum.Item1 == false))
+                {
+                    if (3 <= Math.Abs(napokszama(item.datum.Year, item.datum.Month, item.datum.Day, item.jegyDBvagyLejaratiDatum.Item3.Year, item.jegyDBvagyLejaratiDatum.Item3.Month, item.jegyDBvagyLejaratiDatum.Item3.Day)))
+                        f.WriteLine(item.azonosito + " " + item.jegyDBvagyLejaratiDatum.Item3.ToString("yyyy-MM-dd"));
+                }
+            }
+        }
+
+        public static double napokszama(double e1, double h1, double n1, double e2, double h2, double n2)
+        {
+            h1 = (h1 + 9) / 12.0;
+            e1 = Math.Floor(e1 - h1 / 10);
+            double d1 = 365 * e1 + Math.Floor(e1 / 4) - Math.Floor(e1 / 100) + Math.Floor(e1 / 400) + Math.Floor((h1 * 306 + 5) / 10) + n1 - 1;
+            h2 = (h2 + 9) / 12.0;
+            e2 = (h2 + 9) / 12.0;
+            double d2 = 365 * e2 + Math.Floor(e2 / 4) - Math.Floor(e2 / 100) + Math.Floor(e2 / 400) + Math.Floor((h2 * 306 + 5) / 10) + n2 - 1;
+            return d2-d1;
+        }
+    }
+}
---
+++ c#/érettségire/hiányzások/ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,130 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        class Adat
+        {
+            public DateTime datum { get; set; }
+            public string nev { get; set; }
+            public string hianyzas { get; set; }
+            public Adat(DateTime datum, string nev, string hianyzas)
+            {
+                this.datum = datum;
+                this.nev = nev;
+                this.hianyzas = hianyzas;
+            }
+            public Adat()
+            {
+
+            }
+        }
+
+        static void Main(string[] args)
+        {
+            string[] s = System.IO.File.ReadAllLines("naplo.txt");
+            List<Adat> x = new List<Adat>();
+            DateTime tempDatum = new DateTime();
+            foreach (var item in s)
+            {
+                string[] tempSplit = item.Split(' ');
+                if (tempSplit[0] == "#")
+                    tempDatum = new DateTime(1, int.Parse(tempSplit[1]), int.Parse(tempSplit[2]));
+                else
+                {
+                    Adat a = new Adat();
+                    a.datum = tempDatum;
+                    a.nev = tempSplit[0] + " " + tempSplit[1];
+                    a.hianyzas = tempSplit[2];
+                    x.Add(a);
+                }
+            }
+
+            //1. feladat:
+            Console.WriteLine("1. feladat:");
+
+            //2. feladat:
+            Console.WriteLine("2. feladat:");
+            Console.WriteLine("A naplóban " + x.Count + " bejegyzés van.");
+
+            //3. feladat:
+            Console.WriteLine("3. feladat:");
+
+            long dbIgazolt = 0;
+            long dbIgazolatlan = 0;
+            foreach (var item in x)
+                foreach (var i in item.hianyzas)
+                    if (i == 'X')
+                        ++dbIgazolt;
+                    else if (i == 'I')
+                        ++dbIgazolatlan;
+
+            Console.WriteLine("Az igazolt hiányzások száma " + dbIgazolt + ", az igazolatlanoké " + dbIgazolatlan + " óra.");
+
+            //4. feladat:
+            Console.WriteLine("4. feladat:");
+
+            //5. feladat:
+            Console.WriteLine("5. feladat:");
+            Console.WriteLine("Hónap sorszáma:");
+            int honap = int.Parse(Console.ReadLine());
+            Console.WriteLine("Nap sorszáma:");
+            int nap = int.Parse(Console.ReadLine());
+            Console.WriteLine("Azon a napon " + hetnapja(honap,nap) + " volt.");
+
+            //6. feladat:
+            Console.WriteLine("6. feladat:");
+            Console.WriteLine("A nap neve:");
+            string napNev = Console.ReadLine();
+            Console.WriteLine("Az óra sorszáma:");
+            int oraSorszam = int.Parse(Console.ReadLine());
+
+            long db = 0;
+            foreach (var item in x.Where(k => hetnapja(k.datum.Month, k.datum.Day) == napNev))
+                if (item.hianyzas[oraSorszam - 1] == 'X' || item.hianyzas[oraSorszam - 1] == 'I')
+                    ++db;
+
+            Console.WriteLine("Ekkor összesen " + db + " óra hiányzás történt.");
+
+            //7. feladat:
+            Console.WriteLine("7. feladat:");
+
+            Dictionary<string, long> dic = new Dictionary<string, long>();
+            foreach (var item in x)
+                foreach (var i in item.hianyzas)
+                    if (i == 'X' || i == 'I')
+                    {
+                        if (dic.ContainsKey(item.nev))
+                            ++dic[item.nev];
+                        else
+                            dic.Add(item.nev, 1);
+                    }
+
+            (string, long) max = ("",0);
+            foreach (var item in dic)
+            {
+                if (item.Value > max.Item2)
+                    max = (item.Key, item.Value);
+            }
+
+            Console.Write("A legtöbbet hiányzó tanulók: ");
+            foreach (var item in dic)
+                if(item.Value == max.Item2)
+                    Console.Write(item.Key + " ");
+            Console.WriteLine();
+        }
+
+        public static string hetnapja(int honap, int nap)
+        {
+            string[] napnev = { "vasarnap", "hetfo", "kedd", "szerda", "csutortok", "pentek", "szombat" };
+            int[] napszam = { 0, 31, 59, 90, 120, 151, 181, 212, 243, 273, 304, 335 };
+            int napsorszam = (napszam[honap - 1] + nap) % 7;
+            return napnev[napsorszam];
+        }
+    }
+}
